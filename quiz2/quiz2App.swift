beafe04518c8c7ae7692b59555a0832bb3aac50c
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-20 12:28:05 +0900

    Initial Commit

---
+++ quiz2/quiz2App.swift
@@ -0,0 +1,17 @@
+//
+//  quiz2App.swift
+//  quiz2
+//
+//  Created by 木村朋広 on 2024/02/20.
+//
+
+import SwiftUI
+
+@main
+struct quiz2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
